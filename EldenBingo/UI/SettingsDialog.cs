45d950803fbeda3c441807b402e0cef90105475a
ariel <ariel610@proton.me>
2025-12-09 13:39:50 -0500

    Adds opacity to settings
    This change adds an "opacity" slider to the settings page, so that players can more easily have both the board and their game on a single monitor.

--- EldenBingo/UI/SettingsDialog.cs
+++ EldenBingo/UI/SettingsDialog.cs
@@ -134,6 +134,7 @@ namespace EldenBingo.UI
             _keywordColorAlphaTrackBar.Value = Properties.Settings.Default.KeywordColorsAlpha;
             _keywordColorAlphaTrackBar.ValueChanged += (o, e) => updateKeywordColorText();
 
+            _opacityTrackBar.Value = (int)(Properties.Settings.Default.Opacity * _opacityTrackBar.Maximum);
             updateSizeEnable();
             updatePositionEnable();
             updateMaxSizeEnable();
@@ -247,6 +248,9 @@ namespace EldenBingo.UI
 
             Properties.Settings.Default.LastSettingsTab = tabControl1.SelectedIndex;
 
+            MainForm.Instance.Opacity = Properties.Settings.Default.Opacity =
+                (float)_opacityTrackBar.Value / _opacityTrackBar.Maximum;
+                
             Properties.Settings.Default.Save();
             return true;
         }
@@ -373,6 +377,11 @@ namespace EldenBingo.UI
                 _keywordColors = dialog.Colors;
                 updateKeywordColorText();
             }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
         }
     }
 }
